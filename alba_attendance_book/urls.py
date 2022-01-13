d25183d21d96cbd2e745f6b59e73f11fc04a4173
nomelancholy <starmekey@naver.com>
2022-01-13 22:19:52 +0900

    [U] 부트스트랩 적용

--- alba_attendance_book/urls.py
+++ alba_attendance_book/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from attendance_book import views
+
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
 ]
