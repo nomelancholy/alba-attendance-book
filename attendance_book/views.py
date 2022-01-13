f7df32edd873ae83ab39233688a79cc81436e997
nomelancholy <starmekey@naver.com>
2022-01-14 00:05:43 +0900

    [U] form 생성 / post 요청 / 모델 비교 / message 세팅

--- attendance_book/views.py
+++ attendance_book/views.py
@@ -1,6 +1,28 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
+from .models import Member
+
+
+@csrf_exempt
 def index(request):
-    template = loader.get_template('attendance_book/index.html')
-    return HttpResponse(template.render())
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        birthdate = request.POST['birthdate']
+
+        result = Member.objects.filter(name=name, birthdate=birthdate)
+
+        if result:
+            messages.success(request, "의결권이 있습니다")
+            return redirect('index')
+            print('true')
+        else:
+            messages.warning(request, "의결권이 없습니다")
+            return redirect('index')
+            print('false')
+
+        # return HttpResponse(template.render())
+    else:
+        return render(request, 'attendance_book/index.html')
